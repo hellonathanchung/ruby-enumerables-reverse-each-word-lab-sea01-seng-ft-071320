bf106d3a2600ed5c535ba0ffe325accc07b2759e
Nathan Chung <chung.nathanh@gmail.com>
2020-06-25 00:56:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,2 @@
 def reverse_each_word(sentence)
-  new_sentence = []
-  sentence.split.each do |backwards_words|
-    new_sentence << backwards_words.reverse 
-  end 
-  new_sentence
-end 
-reverse_each_word
+  sentence_to_array = sentence.split 
