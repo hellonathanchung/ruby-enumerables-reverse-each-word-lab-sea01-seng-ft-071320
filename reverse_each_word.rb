c433f6159844897bb494b435b7a89cbeb50da093
Nathan Chung <chung.nathanh@gmail.com>
2020-06-25 00:50:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,10 @@
 def reverse_each_word(string)
   words = string.split(" ")
   sentence = []
+  words.collect do |word|
+    sentence << word.reverse
+  end
+  reversed_setnence.join(" ")
   
   
   
